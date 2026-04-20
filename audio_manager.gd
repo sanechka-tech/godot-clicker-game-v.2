5ba40fd3ec95ac56defb27347cd6f472a25352a6
Leks <joker2011k46@gmail.com>
2026-04-21 03:49:11 +0400

    Допил звука

--- audio_manager.gd
+++ audio_manager.gd
@@ -1,9 +1,11 @@
 extends Node
 
 signal music_mute_changed(is_muted: bool)
+signal music_volume_changed(value: float)
 
 var music_player: AudioStreamPlayer
 var music_muted := false
+var music_volume := 1.0
 
 
 func _ready() -> void:
@@ -12,10 +14,10 @@ func _ready() -> void:
 
 	music_player.stream = load("res://Audio/background_music.ogg")
 	music_player.bus = "Music"
-	music_player.volume_db = 0
 	music_player.play()
 
 	_apply_music_mute()
+	_apply_music_volume()
 
 
 func toggle_music_mute() -> void:
@@ -31,9 +33,28 @@ func set_music_muted(value: bool) -> void:
 	music_mute_changed.emit(music_muted)
 
 
+func set_music_volume(value: float) -> void:
+	var clamped_value := clampf(value, 0.0, 1.0)
+	if is_equal_approx(music_volume, clamped_value):
+		return
+
+	music_volume = clamped_value
+	_apply_music_volume()
+	music_volume_changed.emit(music_volume)
+
+
 func _apply_music_mute() -> void:
 	var music_bus_index := AudioServer.get_bus_index("Music")
 	if music_bus_index == -1:
 		return
 
 	AudioServer.set_bus_mute(music_bus_index, music_muted)
+
+
+func _apply_music_volume() -> void:
+	var music_bus_index := AudioServer.get_bus_index("Music")
+	if music_bus_index == -1:
+		return
+
+	var volume_db := -80.0 if music_volume <= 0.0 else linear_to_db(music_volume)
+	AudioServer.set_bus_volume_db(music_bus_index, volume_db)
