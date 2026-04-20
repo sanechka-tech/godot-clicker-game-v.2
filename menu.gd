61e36521a209d0aadcaba1e7d14331dba6767771
Leks <joker2011k46@gmail.com>
2026-04-20 21:42:50 +0400

    +

--- menu.gd
+++ menu.gd
@@ -1,5 +1,5 @@
 extends Node2D
-
+@onready var settings_panel = $SettingsPanel
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://main_screen.tscn")
@@ -7,3 +7,11 @@ func _on_button_pressed() -> void:
 
 func _on_texture_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://main_screen.tscn")
+
+
+func _on_texture_button_settings_pressed() -> void:
+	settings_panel.visible = true
+
+
+func _on_texture_button_back_pressed() -> void:
+	settings_panel.visible = false
