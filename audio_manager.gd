4c191616a3b5498d09cf9d30e8f01cfb2e361366
Leks <joker2011k46@gmail.com>
2026-04-21 01:37:57 +0400

    +

--- audio_manager.gd
+++ audio_manager.gd
@@ -1,6 +1,9 @@
 extends Node
 
+signal music_mute_changed(is_muted: bool)
+
 var music_player: AudioStreamPlayer
+var music_muted := false
 
 
 func _ready() -> void:
@@ -8,6 +11,29 @@ func _ready() -> void:
 	add_child(music_player)
 
 	music_player.stream = load("res://Audio/background_music.ogg")
-	music_player.bus = "Master"
+	music_player.bus = "Music"
 	music_player.volume_db = 0
 	music_player.play()
+
+	_apply_music_mute()
+
+
+func toggle_music_mute() -> void:
+	set_music_muted(not music_muted)
+
+
+func set_music_muted(value: bool) -> void:
+	if music_muted == value:
+		return
+
+	music_muted = value
+	_apply_music_mute()
+	music_mute_changed.emit(music_muted)
+
+
+func _apply_music_mute() -> void:
+	var music_bus_index := AudioServer.get_bus_index("Music")
+	if music_bus_index == -1:
+		return
+
+	AudioServer.set_bus_mute(music_bus_index, music_muted)
--- menu.gd
+++ menu.gd
@@ -1,5 +1,18 @@
 extends Node2D
-@onready var settings_panel = $SettingsPanel
+
+const MUTE_ON_TEXTURE := preload("res://Images/settings/mute_on.png")
+const MUTE_OFF_TEXTURE := preload("res://Images/settings/mute_off.png")
+
+@onready var settings_panel: Control = $SettingsPanel
+@onready var mute_button: TextureButton = $"SettingsPanel/TextureButton Mute"
+
+
+func _ready() -> void:
+	settings_panel.visible = false
+	mute_button.pressed.connect(_on_texture_button_mute_pressed)
+	AudioManager.music_mute_changed.connect(_on_music_mute_changed)
+	_update_mute_button()
+
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://main_screen.tscn")
@@ -15,3 +28,18 @@ func _on_texture_button_settings_pressed() -> void:
 
 func _on_texture_button_back_pressed() -> void:
 	settings_panel.visible = false
+
+
+func _on_texture_button_mute_pressed() -> void:
+	AudioManager.toggle_music_mute()
+
+
+func _on_music_mute_changed(is_muted: bool) -> void:
+	_update_mute_button(is_muted)
+
+
+func _update_mute_button(is_muted: bool = AudioManager.music_muted) -> void:
+	var current_texture: Texture2D = MUTE_OFF_TEXTURE if is_muted else MUTE_ON_TEXTURE
+	mute_button.texture_normal = current_texture
+	mute_button.texture_pressed = current_texture
+	mute_button.texture_hover = current_texture
